3cb447c856115335881302b03bb9012ae1998471
Aryan Raj <aryan10000raj@gmail.com>
2026-01-20 17:27:44 +0000

    verssion 3

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require("http");
 const PORT = 3000;
 
 http.createServer((req, res) => {
-	res.end("CI/CD Pipeline is working");
+	res.end("Version 3 deployed via Kubernetes\n");
 }).listen(PORT, () => {
 	console.log(`Server is running on port ${PORT}`);
 });
